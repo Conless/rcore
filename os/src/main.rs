2877a49028e8f4bb2004f460864f3f07dc8a0926
Yi Pan <conlesspan@outlook.com>
2024-03-15 10:29:41 +0800

    fix some bugs on qemu

--- os/src/main.rs
+++ os/src/main.rs
@@ -8,7 +8,7 @@
 //! We then call [`println!`] to display `Hello, world!`.
 
 #![deny(missing_docs)]
-#![deny(warnings)]
+// #![deny(warnings)]
 #![no_std]
 #![no_main]
 #![feature(panic_info_message)]
@@ -36,30 +36,30 @@ pub fn clear_bss() {
 /// the rust entry-point of os
 #[no_mangle]
 pub fn rust_main() -> ! {
-    extern "C" {
-        fn stext(); // begin addr of text segment
-        fn etext(); // end addr of text segment
-        fn srodata(); // start addr of Read-Only data segment
-        fn erodata(); // end addr of Read-Only data ssegment
-        fn sdata(); // start addr of data segment
-        fn edata(); // end addr of data segment
-        fn sbss(); // start addr of BSS segment
-        fn ebss(); // end addr of BSS segment
-        fn boot_stack(); // stack bottom
-        fn boot_stack_top(); // stack top
-    }
+    // extern "C" {
+    //     fn stext(); // begin addr of text segment
+    //     fn etext(); // end addr of text segment
+    //     fn srodata(); // start addr of Read-Only data segment
+    //     fn erodata(); // end addr of Read-Only data ssegment
+    //     fn sdata(); // start addr of data segment
+    //     fn edata(); // end addr of data segment
+    //     fn sbss(); // start addr of BSS segment
+    //     fn ebss(); // end addr of BSS segment
+    //     fn boot_stack(); // stack bottom
+    //     fn boot_stack_top(); // stack top
+    // }
     clear_bss();
     println!("Hello, world!");
-    println!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
-     println!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
-    println!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
-    println!(
-         "boot_stack [{:#x}, {:#x})",
-         boot_stack as usize, boot_stack_top as usize
-     );
-    println!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    // println!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
+    //  println!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+    // println!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
+    // println!(
+    //      "boot_stack [{:#x}, {:#x})",
+    //      boot_stack as usize, boot_stack_top as usize
+    //  );
+    // println!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
     
-    QEMU_EXIT_HANDLE.exit_success();  //need successful qemu exit
+    // QEMU_EXIT_HANDLE.exit_success();  //need successful qemu exit
     //QEMU_EXIT_HANDLE.exit_failure(); //need failed qemu exit
-    //panic!("Shutdown machine!");
+    panic!("Shutdown machine!");
 }
